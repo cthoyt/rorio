bb41d45cc197c1b003cf0f581b41a1c6f9167944
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-05-07 23:55:55 +0200

    Update data to ROR v2.0 and add notice

--- README.md
+++ README.md
@@ -1,50 +1,49 @@
 # ROR in OWL
 
-Similar to the [ORCIDIO](https://github.com/cthoyt/orcidio), which makes an ontology of instances of researchers via
-ORCID, this repository houses a script to convert the [Research Organization Registry (ROR)](https://ror.org) into
-an ontology of instances of organizations.
+> [!WARNING]
+> This repository originally contained code for converting the
+> [Research Organization Registry (ROR)](https://ror.org) into an ontology of
+> instances. The code has now been upstreamed in the following two places:
+>
+> 1. The code for automatically interacting with Zenodo to download the latest
+>    (or an explicitly requested) version of ROR is now in
+>    https://github.com/cthoyt/ror-downloader
+> 2. The code for constructing an ontology from ROR is now part of PyOBO, which
+>    is a bigger suite for database to ontology conversions. See
+>    https://github.com/biopragmatics/pyobo/blob/main/src/pyobo/sources/ror.py.
+>
+> The OBO DB Ingest project at https://github.com/biopragmatics/obo-db-ingest
+> now stores the results of many database to ontology conversions. The ROR
+> resources are listed under
+> https://github.com/biopragmatics/obo-db-ingest/tree/main/export/ror#readme.
+>
+> Eventually, I would like to forward the old _rorio_ PURLs to point to the
+> associated _obo-db-ingest_ PURLs as described in the table below.
 
-The latest version can be downloaded with the following:
-
-| Format         | PURL                              |
-|----------------|-----------------------------------|
-| OWL RDF/XML    | https://w3id.org/rorio/rorio.owl  |
-| OBO Flat File  | https://w3id.org/rorio/rorio.obo  |
-| OBO Graph JSON | https://w3id.org/rorio/rorio.json |
-
+| Format         | Legacy PURL                          | New PURL                                                 |
+|----------------|--------------------------------------|----------------------------------------------------------|
+| OWL Functional | https://w3id.org/rorio/rorio.ofn.gz  |                                                          |
+| OWL RDF/XML    | https://w3id.org/rorio/rorio.owl.gz  | https://w3id.org/biopragmatics/resources/ror/ror.owl.gz  |
+| OBO Flat File  | https://w3id.org/rorio/rorio.obo.gz  | https://w3id.org/biopragmatics/resources/ror/ror.obo     |
+| OBO Graph JSON | https://w3id.org/rorio/rorio.json.gz | https://w3id.org/biopragmatics/resources/ror/ror.json.gz |
 
 ![](img/rorio-in-protege.png)
 
-In the screenshot, you can see the `part of`, `has part`, and `located in` relations for the example organization. Unfortunately, Protégé does not have support for navigating partonomies on individuals.
+In the screenshot, you can see the `has suborganization`,
+`is suborganization of`, and `located in` relations for the example
+organization.
 
 ## Build
 
-Install the requirements and run with:
+The build script is now a thin wrapper around PyOBO code. It can be run with
+`uv`:
 
 ```shell
-python -m pip install -r requirements.txt
-python build.py
-```
-
-## Named Entity Recognition
-
-A [Gilda](https://github.com/gyorilab/gilda) index is generated for usage in named entity recognition (NER)
-and named entity normalization (NEN; i.e., grounding). It is available at https://w3id.org/rorio/rorio.gilda.tsv.gz.
-It can be used like in:
-
-```python
-from urllib.request import urlretrieve
-from gilda import Grounder
-
-url = "https://w3id.org/rorio/rorio.gilda.tsv.gz"
-path = "rorio.gilda.tsv.gz"
-urlretrieve(url, path)
-
-grounder = Grounder(path)
-scored_matches = grounder.ground("Rheinische Friedrich-Wilhelms-Universität Bonn")
+uv run build.py
 ```
 
 ## License
 
-The data downloaded from https://doi.org/10.5281/zenodo.6347574 are licensed under CC0. So is this repo. This is
-additionally self-documented in the ontology files.
+The data downloaded from https://doi.org/10.5281/zenodo.6347574 are licensed
+under CC0. So is this repo. This is additionally self-documented in the ontology
+files.
--- build.py
+++ build.py
@@ -1,315 +1,36 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "click>=8.3.3",
+#     "pyobo>=0.12.21",
+# ]
+# ///
+
 import json
-import os
-import zipfile
 from pathlib import Path
 
-import bioregistry
 import click
-import zenodo_client
-from funowl import (
-    Annotation,
-    AnnotationAssertion,
-    Class,
-    ClassAssertion,
-    NamedIndividual,
-    ObjectProperty,
-    ObjectPropertyAssertion,
-    Ontology,
-    OntologyDocument,
-)
-from rdflib import DCTERMS, OWL, RDFS, Literal, Namespace, URIRef
-from tqdm import tqdm
-from gilda import Term
-from gilda.term import dump_terms
-from gilda.process import normalize
 
-# Paths and URLs
+from pyobo.sources.ror import get_ror_to_country_geonames, RORGetter
+
 HERE = Path(__file__).parent.resolve()
 OFN_PATH = HERE.joinpath("rorio.ofn")
+OBO_PATH = HERE.joinpath("rorio.obo")
 OWL_PATH = HERE.joinpath("rorio.owl")
 JSON_PATH = HERE.joinpath("rorio.json")
-GILDA_PATH = HERE.joinpath("rorio.gilda.tsv.gz")
 ROR_TO_COUNTRIES = HERE.joinpath("countries.json")
-
-# Namespaces
-ORCID = Namespace("https://orcid.org/")
-ROR = Namespace("https://ror.org/")
-GEONAMES = Namespace("https://www.geonames.org/")
-OBO = Namespace("http://purl.obolibrary.org/obo/")
-BFO = Namespace("http://purl.obolibrary.org/obo/BFO_")
-ENVO = Namespace("http://purl.obolibrary.org/obo/ENVO_")
-OBI = Namespace("http://purl.obolibrary.org/obo/OBI_")
-RO = Namespace("http://purl.obolibrary.org/obo/RO_")
-OMO = Namespace("http://purl.obolibrary.org/obo/OMO_")
-OIO = Namespace("http://www.geneontology.org/formats/oboInOwl#")
-
-# Constants
-CITY_CLASS = ENVO["00000856"]
-ORG_CLASS = OBI["0000245"]
-LOCATED_IN = RO["0001025"]
-PART_OF = BFO["0000050"]
-HAS_PART = BFO["0000051"]
-SUCCESSOR = BFO["0000063"]
-PREDECESSOR = BFO["0000062"]
-CHARLIE = ORCID["0000-0003-4423-4370"]
-RMAP = {
-    "Related": RDFS.seeAlso,
-    "Child": HAS_PART,
-    "Parent": PART_OF,
-    "Predecessor": PREDECESSOR,
-    "Successor": SUCCESSOR,
-}
-NAME_REMAPPING = {
-    "'s-Hertogenbosch": "Den Bosch",  # SMH Netherlands, why u gotta be like this
-    "'s Heeren Loo": "s Heeren Loo",
-    "'s-Heerenberg": "s-Heerenberg",
-    "Institut Virion\\Serion": "Institut Virion/Serion",
-    "Hematology\\Oncology Clinic": "Hematology/Oncology Clinic",
-}
-
-ONTOLOGY_URI = "https://w3id.org/rorio/rorio.owl"
-
-#: Zenodo ID for ROR
-PERMENANT = "10086202"
-
-
-def get_latest():
-    client = zenodo_client.Zenodo()
-    latest_record_id = client.get_latest_record(PERMENANT)
-    response = client.get_record(latest_record_id)
-    response_json = response.json()
-    version = response_json["metadata"]["version"].lstrip("v")
-    file_record = response_json["files"][0]
-    name = file_record["key"]
-    url = file_record["links"]["self"]
-    path = client.download(latest_record_id, name=name)
-    with zipfile.ZipFile(path) as zf:
-        for zip_info in zf.filelist:
-            if zip_info.filename.endswith(".json"):
-                with zf.open(zip_info) as file:
-                    return version, url, json.load(file)
-    raise FileNotFoundError
 
 
 @click.command()
-@click.option("--quiet", is_flag=True)
-def main(quiet: bool):
-    version, source_uri, records = get_latest()
-    unhandled_xref_prefixes = set()
+def main() -> None:
+    ROR_TO_COUNTRIES.write_text(json.dumps(get_ror_to_country_geonames(), indent=2, sort_keys=True))
 
-    ontology = Ontology(iri=URIRef(ONTOLOGY_URI))
-    ontology.annotations.extend(
-        (
-            Annotation(DCTERMS.title, "ROR in OWL"),
-            Annotation(DCTERMS.creator, CHARLIE),
-            Annotation(DCTERMS.license, "https://creativecommons.org/publicdomain/zero/1.0/"),
-            Annotation(RDFS.seeAlso, "https://github.com/cthoyt/rorio"),
-            Annotation(OWL.versionInfo, Literal(version)),
-            Annotation(DCTERMS.source, URIRef(source_uri)),
-        )
-    )
-
-    ontology.declarations(
-        Class(CITY_CLASS),
-        Class(ORG_CLASS),
-        ObjectProperty(LOCATED_IN),
-        *(ObjectProperty(p) for p in RMAP.values()),
-    )
-    ontology.annotations.extend(
-        [
-            AnnotationAssertion(RDFS.label, CITY_CLASS, "city"),
-            AnnotationAssertion(RDFS.label, ORG_CLASS, "organization"),
-            AnnotationAssertion(RDFS.label, LOCATED_IN, "located in"),
-            AnnotationAssertion(RDFS.label, PART_OF, "part of"),
-            AnnotationAssertion(RDFS.label, HAS_PART, "has part"),
-            AnnotationAssertion(RDFS.label, SUCCESSOR, "precedes"),
-            AnnotationAssertion(RDFS.label, PREDECESSOR, "preceded by"),
-            AnnotationAssertion(RDFS.label, RDFS.seeAlso, "see also"),
-        ]
-    )
-
-    terms: list[Term] = []
-
-    def _add_term(t, luid, entry_name, status):
-        terms.append(
-            Term(
-                norm_text=normalize(t),
-                text=t,
-                db="ror",
-                id=luid,
-                entry_name=entry_name,
-                status=status,
-                source="ror",
-            )
-        )
-
-    ror_to_country = {}
-    for record in tqdm(
-        records,
-        unit_scale=True,
-        unit="record",
-        desc=f"ROR v{version} to OWL",
-        disable=quiet,
-    ):
-        organization_uri_ref = URIRef(record["id"])
-        organization_luid = record["id"].removeprefix("https://ror.org/")
-        organization_name = record["name"]
-        organization_name = NAME_REMAPPING.get(organization_name, organization_name)
-
-        ontology.declarations(NamedIndividual(organization_uri_ref))
-        try:
-            ontology.annotations.extend(
-                [
-                    AnnotationAssertion(
-                        RDFS.label,
-                        organization_uri_ref,
-                        Literal(organization_name),
-                    ),
-                    ClassAssertion(ORG_CLASS, organization_uri_ref),
-                ]
-            )
-        except (TypeError, AssertionError):
-            tqdm.write(f"failed on organization: {organization_name} ({organization_uri_ref})")
-            continue
-        _add_term(organization_name, organization_luid, organization_name, "name")
-
-        for address in record.get("addresses", []):
-            city = address.get("geonames_city")
-            if not city:
-                continue
-            # If there is an organization with multiple countries, this will squash
-            # all but one. It's a reasonable simplficiation, though
-            ror_to_country[organization_luid] = str(address["country_geonames_id"])
-            city_uri_ref = GEONAMES[str(city["id"])]
-            city_name = city["city"]
-            city_name = NAME_REMAPPING.get(city_name, city_name)
-            ontology.declarations(NamedIndividual(city_uri_ref))
-            try:
-                ontology.annotations.extend(
-                    [
-                        ObjectPropertyAssertion(LOCATED_IN, organization_uri_ref, city_uri_ref),
-                        AnnotationAssertion(
-                            RDFS.label,
-                            city_uri_ref,
-                            Literal(city_name),
-                            # [
-                            #     Annotation(DC.source, URIRef("https://geonames.org/")),
-                            #     Annotation(
-                            #         DCTERMS.license, URIRef(city["license"]["license"])
-                            #     ),
-                            # ],
-                        ),
-                        ClassAssertion(CITY_CLASS, city_uri_ref),
-                    ]
-                )
-            except AssertionError:
-                tqdm.write(f"[{organization_uri_ref}] failed on city: {city_name} ({city_uri_ref})")
-                continue
-
-        for relationship in record.get("relationships", []):
-            ontology.annotations.append(
-                AnnotationAssertion(
-                    RMAP[relationship["type"]],
-                    organization_uri_ref,
-                    URIRef(relationship["id"]),
-                )
-            )
-
-        for synonym in record.get("aliases", []):
-            try:
-                ontology.annotations.append(
-                    AnnotationAssertion(
-                        OIO["hasExactSynonym"], organization_uri_ref, Literal(synonym)
-                    )
-                )
-            except (AssertionError, TypeError):
-                tqdm.write(
-                    f"bad synonym for {organization_name} ({organization_uri_ref}): {synonym}"
-                )
-                continue
-            _add_term(synonym, organization_luid, organization_name, "synonym")
-
-        for acronym in record.get("acronyms", []):
-            try:
-                ontology.annotations.append(
-                    AnnotationAssertion(
-                        OIO["hasExactSynonym"],
-                        organization_uri_ref,
-                        Literal(acronym),
-                        [
-                            Annotation(OIO["SynonymType"], OMO["0003000"]),
-                        ],
-                    )
-                )
-            except (AssertionError, TypeError):
-                tqdm.write(
-                    f"bad acronym for {organization_name} ({organization_uri_ref}): {acronym}"
-                )
-                continue
-            # Acronyms cause collisions with country and city acronyms, skip for now
-            # _add_term(acronym, organization_luid, organization_name, "synonym")
-
-        for prefix, xref_data in record.get("external_ids", {}).items():
-            if prefix == "OrgRef":
-                # OrgRef refers to wikipedia page id, see
-                # https://stackoverflow.com/questions/6168020/what-is-wikipedia-pageid-how-to-change-it-into-real-page-url
-                continue
-            norm_prefix = bioregistry.normalize_prefix(prefix)
-            if norm_prefix is None:
-                if prefix not in unhandled_xref_prefixes:
-                    tqdm.write(
-                        f"Unhandled prefix: {prefix} in {organization_name} ({organization_uri_ref}). Values:"
-                    )
-                    for xref_id in xref_data["all"]:
-                        tqdm.write(f"- {xref_id}")
-                    unhandled_xref_prefixes.add(prefix)
-                continue
-
-            identifiers = xref_data["all"]
-            if isinstance(identifiers, str):
-                identifiers = [identifiers]
-            for xref_id in identifiers:
-                ontology.annotations.append(
-                    AnnotationAssertion(
-                        OIO["hasDbXref"],
-                        organization_uri_ref,
-                        Literal(bioregistry.curie_to_str(norm_prefix, xref_id.replace(" ", ""))),
-                    )
-                )
-
-    ROR_TO_COUNTRIES.write_text(json.dumps(ror_to_country, indent=2))
-
-    doc = OntologyDocument(
-        ontology=ontology,
-        orcid=ORCID,
-        obo=OBO,
-        ror=ROR,
-        dcterms=DCTERMS._NS,
-        owl=OWL._NS,
-        geonames=GEONAMES,
-        oio=OIO,
-        BFO=BFO,
-        RO=RO,
-    )
-    click.echo(f"writing to {OFN_PATH}")
-    OFN_PATH.write_text(f"{doc}\n")
-
-    # TODO add prefix file for conversion to shorten files
-    cmd = f"robot convert --input {OFN_PATH} --output {OFN_PATH}"
-    click.secho("Canonicalizing OFN")
-    click.secho(cmd, fg="green")
-    os.system(cmd)
-
-    cmd = f"robot convert --input {OFN_PATH} --output {OWL_PATH}"
-    click.secho(cmd, fg="green")
-    os.system(cmd)
-
-    cmd = f"robot convert --input {OFN_PATH} --output {JSON_PATH}"
-    click.secho(cmd, fg="green")
-    os.system(cmd)
-
-    click.secho(f"writing Gilda index to {GILDA_PATH}", fg="green")
-    dump_terms(terms, GILDA_PATH)
+    ontology = RORGetter()
+    ontology.write_obograph(JSON_PATH)
+    ontology.write_owl(OWL_PATH)
+    ontology.write_ofn(OFN_PATH)
+    ontology.write_obo(OBO_PATH)
+    # TODO synonyms?
 
 
 if __name__ == "__main__":
